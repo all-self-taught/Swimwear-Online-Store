8e5186d1769c8b4f8fa4bca066394f0059958990
tarasabella <tarasabella@gmail.com>
2019-04-10 18:00:05 -0400

    css changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,50 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./styles.css";
+import Carousel from "./Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const LandingPage = () => {
   return (
-    <div className="showcase">
-      <a class="showcase" href="#two!">
-        <img src="https://d1hano0onsuhm2.cloudfront.net/wp-content/uploads/2019/02/16102741/FrankiesBikinis-Ali-SpringBloom.jpeg" />
-      </a>
-      <div className="center-box">
-        <div className="white-box hpTitle">
-          <h1 id="hpTitle">Aloha Babes</h1>
+    <div className="container">
+      <header id="showcase">
+        <p>Say aloha to our new bikini collection. </p>
+        <h1>
+          <Link to="/shop">
+            <a href="#" class="button">
+              SHOP THE COLLECTION
+            </a>
+          </Link>
+        </h1>
+      </header>
+      <section id="section-a">
+        <div>
+          <Carousel />
         </div>
-        <div className="row">
-          <div className="col l3 offset-l2 m5 offset-m1 s10 offset-s1">
-            <Link to="/shop">
-              <h4 className="white-box">
-                <strong>SHOP THE COLLECTION</strong>
-              </h4>
-            </Link>
-          </div>
-          <div class="container">
-            <h3>#alohababes</h3>
-            <div
-              class="carousel carousel-slider"
-              id="demo-carousel-auto"
-              data-indicators="true"
-            >
-              <a class="carousel-item" href="#one!">
-                <img src="images/banana.jpg" />
-              </a>
-              <a class="carousel-item" href="#two!">
-                <img src="images/mango.jpg" />
-              </a>
-              <a class="carousel-item" href="#three!">
-                <img src="images/strawberries.jpg" />
-              </a>
-              <a class="carousel-item" href="#four!">
-                <img src="images/currant.jpg" />
-              </a>
-              <a class="carousel-item" href="#five!">
-                <img src="images/mango.jpg" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      </section>
+      <section id="section-b">
+        <p>
+          <h1>
+            <center>luxe swimwear </center>
+          </h1>
+          If you’re looking for versatile swimwear that can seamlessly
+          transition from beach to street, look no further than our designs. Our
+          exclusive suits blend top clothing trends with comfortable cuts
+          intended to show off your assets. Not only are our luxe bikinis
+          effortlessly chic, they also capture a beachy vibe in styles that can
+          easily transition to stylish ensembles. Aloha Babe Bikinis is equally
+          as committed to quality swimwear as we are to trendy designs. With our
+          suits, you can always count on luxurious fabrics, well-made bikini
+          swimwear that is built to last, flattering and versatile shapes
+          suitable for a variety of body types. But don’t just take our word for
+          it—shop our luxe swimwear collection and find this year’s perfect suit
+          for the beach and beyond.
+        </p>
+      </section>
     </div>
   );
 };
